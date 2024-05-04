c86b728e564eaf6f02ddd611780c84a3c085baae
Paolo <pauloportes.guimalan187@gmail.com>
2024-05-04 13:18:35 +0800

    feat: kernel backdoor has now been separated repo

--- renderer/kernel/utils/connection.ts
+++
@@ -1,7 +0,0 @@
-export default {
-    url: `mongodb+srv://dt187:demonterror187@cluster0.6uzwm.mongodb.net/neonpos?w=majority`,
-    params:{
-        useNewUrlParser: true,
-        useUnifiedTopology: true
-    }
-}
